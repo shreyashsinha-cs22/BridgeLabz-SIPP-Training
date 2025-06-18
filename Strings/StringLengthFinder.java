ca24eb13973f84afbc0a09fafcfcfe95bb163f64
shreyashsinha-cs22 <shreyashsinha0017@gmail.com>
2025-06-19 00:31:04 +0530

    [Shreyash] add. checing the length of the sentence without using inbuilt method

---
+++ Strings/StringLengthFinder.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class StringLengthFinder {
+    // Method to get string length without using .length() and without try-catch
+    public static int getStringLength(String str) {
+        char[] chars = str.toCharArray(); // convert string to char array
+        int count = 0;
+
+        for (char ch : chars) {
+            count++;
+        }
+
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String input = scanner.next();
+        int manualLength = getStringLength(input);
+        int builtinLength = input.length(); 
+        System.out.println("Manual length: " + manualLength);
+        System.out.println("Built-in length (.length())            : " + builtinLength);
+    }
+}
