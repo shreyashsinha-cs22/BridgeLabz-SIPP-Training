99c131c145c60359f9a5236e5b9417fc716e5a0b
shreyashsinha-cs22 <shreyashsinha0017@gmail.com>
2025-06-25 08:40:35 +0530

    remove core-java folder

--- java_fundamentals/question4.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class question4{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		int radius = sc.nextInt();
-		double Area = 3.14 * radius * radius;
-		System.out.println(Area);
-	}
-}
