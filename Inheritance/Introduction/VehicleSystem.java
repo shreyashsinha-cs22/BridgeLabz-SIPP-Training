8e9d94301d3b5af7e794ccedb480a9bdc3047867
shreyashsinha-cs22 <shreyashsinha0017@gmail.com>
2025-07-03 12:21:45 +0530

    [Shreyash] Add. displaying vehicles details

---
+++ Inheritance/Introduction/VehicleSystem.java
@@ -0,0 +1,81 @@
+// Superclass
+class Vehicle {
+    int maxSpeed;
+    String fuelType;
+
+    public Vehicle(int maxSpeed, String fuelType) {
+        this.maxSpeed = maxSpeed;
+        this.fuelType = fuelType;
+    }
+
+    public void displayInfo() {
+        System.out.println("Max Speed: " + maxSpeed + " km/h");
+        System.out.println("Fuel Type: " + fuelType);
+    }
+}
+
+// Subclass: Car
+class Car extends Vehicle {
+    int seatCapacity;
+
+    public Car(int maxSpeed, String fuelType, int seatCapacity) {
+        super(maxSpeed, fuelType);
+        this.seatCapacity = seatCapacity;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Seat Capacity: " + seatCapacity);
+    }
+}
+
+// Subclass: Truck
+class Truck extends Vehicle {
+    int loadCapacity; // in tons
+
+    public Truck(int maxSpeed, String fuelType, int loadCapacity) {
+        super(maxSpeed, fuelType);
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Load Capacity: " + loadCapacity + " tons");
+    }
+}
+
+// Subclass: Motorcycle
+class Motorcycle extends Vehicle {
+    boolean hasCarrier;
+
+    public Motorcycle(int maxSpeed, String fuelType, boolean hasCarrier) {
+        super(maxSpeed, fuelType);
+        this.hasCarrier = hasCarrier;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Has Carrier: " + (hasCarrier ? "Yes" : "No"));
+    }
+}
+
+// Main class to demonstrate polymorphism
+public class VehicleSystem {
+    public static void main(String[] args) {
+        // Polymorphic array of Vehicle type
+        Vehicle[] vehicles = new Vehicle[3];
+
+        vehicles[0] = new Car(180, "Petrol", 5);
+        vehicles[1] = new Truck(120, "Diesel", 10);
+        vehicles[2] = new Motorcycle(100, "Petrol", true);
+
+        // Polymorphic behavior: each subclass's displayInfo() is called
+        for (int i = 0; i < vehicles.length; i++) {
+            System.out.println("\n--- Vehicle " + (i + 1) + " Info ---");
+            vehicles[i].displayInfo();
+        }
+    }
+}
